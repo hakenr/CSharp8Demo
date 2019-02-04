34beda12ca9f0b9b8733e05642cc9a6e8b445afd
Robert Haken <haken@havit.cz>
2019-02-04 01:09:30 +0100

    Default implementations of interface members

---
+++ CSharp8Demo/09_DefaultInterfaceMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haken.CSharp8Demo
+{
+	public class DefaultInterfaceMethods
+	{
+		// Not released in Preview yet.
+	}
+
+	interface ILogger
+	{
+		void Log(LogLevel level, string message);
+		// public void Log(Exception ex) => Log(LogLevel.Error, ex.ToString()); // New overload
+	}
+
+	public class ConsoleLogger : ILogger
+	{
+		public void Log(LogLevel level, string message) {  }
+		// Log(Exception) gets default implementation
+	}
+
+	public enum LogLevel
+	{
+		Info,
+		Error
+	}
+}
