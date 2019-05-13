ccb21dc3e54f3d909f8065b1b3a5ae6ca3251a78
Robert Haken <haken@havit.cz>
2019-05-13 23:18:43 +0200

    VS2019 - 02_Refactorings_TupleToStruct.cs - cursor position

--- VS2019Demo/02_Refactorings_TupleToStruct.cs
+++ VS2019Demo/02_Refactorings_TupleToStruct.cs
@@ -8,6 +8,8 @@ namespace VS2019Demo
 {
 	public class Refactorings_TupleToStruct
 	{
+		// cursor position :-)
+		// ___vv______________
 		public (int x, int y) GetCoordinates()
 		{
 			return (1, 1);
