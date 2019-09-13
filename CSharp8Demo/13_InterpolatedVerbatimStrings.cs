d53a0cea37aa078f14de3b5f277e1a4aa900cb62
Robert Haken <haken@havit.cz>
2019-09-13 17:50:25 +0200

    Interpolated verbatim strings

---
+++ CSharp8Demo/13_InterpolatedVerbatimStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haken.CSharp8Demo
+{
+	public class InterpolatedVerbatimStrings
+	{
+		public void Demo()
+		{
+			string s;
+
+			s = "D:\\Demo"; // string
+			s = @"D:\Demo"; // @ = verbatim string
+			s = $"Today {DateTime.Today:g}"; // $ = interpolated string
+
+			s = $@"C# 7 interpolated verbatim string";
+
+			s = @$"C# 8 interpolated verbatim string";
+
+
+			Console.WriteLine(s);
+		}
+	}
+}
