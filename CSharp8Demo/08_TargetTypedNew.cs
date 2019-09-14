4cd8a4aeb543fcaa9dacf18d2cb57e56853bbdf2
Robert Haken <haken@havit.cz>
2019-09-15 00:02:11 +0200

    TargetTypedNew => C# 9.0 candidate

--- CSharp8Demo/08_TargetTypedNew.cs
+++ CSharp8Demo/08_TargetTypedNew.cs
@@ -15,7 +15,7 @@ namespace Haken.CSharp8Demo
 			// Current C#
 			using (var reader = XmlReader.Create("https://knowledge-base.havit.eu/feed/", new XmlReaderSettings() { IgnoreWhitespace = true })) { }
 
-			// Won't make it into the C# 8.0
+			// Won't make it into the C# 8.0 - currently "C# 9.0 Candidate"
 			// using var reader2 = XmlReader.Create("https://knowledge-base.havit.eu/feed/", new() { IgnoreWhitespace = true });
 
 			// Point[] ps = { new (1, 4), new (3,-2), new (9, 5) };
